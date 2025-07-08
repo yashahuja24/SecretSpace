461b5a5d34d6eb4996c77d1443aa410bfdc631ba
yashahuja24 <yashahuja2004@gmail.com>
2025-07-09 04:53:12 +0530

    Added Cors for connection with vercel

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
