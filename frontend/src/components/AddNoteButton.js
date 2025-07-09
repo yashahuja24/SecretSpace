5b56a3afdd545f1d51d7a84f72e4e9a82dd3161e
yashahuja24 <yashahuja2004@gmail.com>
2025-07-09 12:54:04 +0530

    scrollto up on +addnote button

--- frontend/src/components/AddNoteButton.js
+++ frontend/src/components/AddNoteButton.js
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 const AddNoteButton = () => {
+  const location = useLocation();
+  const handleClick = () => {
+    if (location.pathname === "/") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
   return (
     <div className="flex justify-left mt-3">
       <Link
         to="/"
+        onClick={handleClick}
         className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-6 rounded-xl shadow-lg transition duration-300"
       >
         + Add Note
@@ -11,4 +19,5 @@ const AddNoteButton = () => {
     </div>
   );
 };
+
 export default AddNoteButton;
