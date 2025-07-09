d52aefcbe1e4bdad284551ad02540ec0a05d7bd2
yashahuja24 <yashahuja2004@gmail.com>
2025-07-09 05:49:42 +0530

    altered link

--- frontend/src/components/AddNoteButton.js
+++ frontend/src/components/AddNoteButton.js
@@ -3,7 +3,7 @@ const AddNoteButton = () => {
   return (
     <div className="flex justify-left mt-3">
       <Link
-        to="/#add-note"
+        to="/"
         className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-6 rounded-xl shadow-lg transition duration-300"
       >
         + Add Note
